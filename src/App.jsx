8e03147ea78bcd1f2bdb5324e601a7a7c3f05b06
Josue M Darce <133271041+darcejosue@users.noreply.github.com>
2024-04-15 21:11:08 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1>Pagina en proceso de construcción</h1>
+        <img src='/logo.png' width={300}/>
+
+    </>
+  )
+}
+
+export default App
